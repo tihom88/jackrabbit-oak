5abca70ba49ccdd6e424ed3d6c0c0d18c3b268a3
Nitin Gupta <nitigup@adobe.com>
2023-10-03 15:51:49 +0530

    change parameter name

--- oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/IndexerSupport.java
+++ oak-run-commons/src/main/java/org/apache/jackrabbit/oak/index/IndexerSupport.java
@@ -225,11 +225,11 @@ public class IndexerSupport {
     /**
      *
      * @param indexDefinitions set of IndexDefinition to be used to calculate the Path Predicate
-     * @param typeToString Function to convert type <T> to path of type <String>
+     * @param typeToRepositoryPath Function to convert type <T> to valid repository path of type <String>
      * @param <T>
      * @return filter predicate based on the include/exclude path rules of the given set of index definitions.
      */
-    public <T> Predicate<T> getFilterPredicate(Set<IndexDefinition> indexDefinitions, Function<T, String> typeToString) {
-        return t -> indexDefinitions.stream().anyMatch(indexDef -> indexDef.getPathFilter().filter(typeToString.apply(t)) != PathFilter.Result.EXCLUDE);
+    public <T> Predicate<T> getFilterPredicate(Set<IndexDefinition> indexDefinitions, Function<T, String> typeToRepositoryPath) {
+        return t -> indexDefinitions.stream().anyMatch(indexDef -> indexDef.getPathFilter().filter(typeToRepositoryPath.apply(t)) != PathFilter.Result.EXCLUDE);
     }
 }
